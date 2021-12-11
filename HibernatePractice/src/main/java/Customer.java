3c9362e83d70493c0965fb0ccc7d3229babba0ed
mohamad <mohamaddhs7@gmail.com>
2021-12-11 09:58:40 +0330

    9:58 AM 12/11/2021

---
+++ HibernatePractice/src/main/java/Customer.java
@@ -0,0 +1,95 @@
+import org.hibernate.annotations.Type;
+import org.hibernate.type.BigDecimalType;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "cust_code")
+    private long id;
+    @Column(name = "cust_name")
+    private String name;
+    @Column(name = "cust_city")
+    private String city;
+    @Column(name = "working_area")
+    private String workingArea;
+    @Column(name = "cust_country")
+    private String country;
+    @Column(name = "grade")
+    private BigDecimal grade;
+   @Column(scale = 12,precision = 2,name = "payment_amt")
+    private BigDecimal paymentAmount;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getWorkingArea() {
+        return workingArea;
+    }
+
+    public void setWorkingArea(String workingArea) {
+        this.workingArea = workingArea;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public BigDecimal getGrade() {
+        return grade;
+    }
+
+    public void setGrade(BigDecimal grade) {
+        this.grade = grade;
+    }
+
+    public BigDecimal getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(BigDecimal paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", workingArea='" + workingArea + '\'' +
+                ", country='" + country + '\'' +
+                ", grade=" + grade +
+                ", paymentAmount=" + paymentAmount +
+                '}';
+    }
+}
--- HibernatePractice/src/main/java/Main.java
+++ HibernatePractice/src/main/java/Main.java
@@ -1,7 +1,14 @@
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.*;
+import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,17 +19,71 @@ public class Main {
 //        e1.setMarriage(true);
 //        e1.setSalary(2000);
 
+//        Customer c1 = new Customer();
+//        //c1.setId("C00026");
+//        c1.setCity("Tehran");
+//        c1.setCountry("Iran");
+//        c1.setGrade(BigDecimal.valueOf(4));
+//        c1.setName("Ali");
+//        c1.setPaymentAmount(BigDecimal.valueOf(5500.00));
+//        c1.setWorkingArea("Argentina");
+
         SessionFactory sf=new Configuration().configure().buildSessionFactory();
         Session session=sf.getCurrentSession();
         Transaction tx= session.beginTransaction();
 
+//        Criteria cr = session.createCriteria(Customer.class);
+//        ProjectionList projectionList = Projections.projectionList();
+//        projectionList.add(Projections.alias(Projections.property("workingArea"),"workingArea"));
+//        projectionList.add(Projections.alias(Projections.sum("paymentAmount"),"sum"));
+//        projectionList.add(Projections.groupProperty("workingArea"));
+//        cr.setProjection(projectionList);
+//        Criterion co =Restrictions.eq("workingArea","Torento");
+//        cr.add(co);
+//        cr.setResultTransformer(cr.ALIAS_TO_ENTITY_MAP);
+//        List Result =  cr.list();
+
+//        Query q =session.createQuery("select c.workingArea , sum (c.paymentAmount) from Customer as c where c.workingArea='Torento'group by c.workingArea");
+//        Map<String,Double> customers = (Map) q.list();
+//        System.out.println(customers.size());
+
+//        Query q =session.createQuery("select c.workingArea , sum (c.paymentAmount) from Customer as c where c.workingArea='Torento'group by c.workingArea");
+//        Map<String,Long> map1= (Map<String, Long>) q.list();
+//        System.out.println(map1.size());
+
+
+
+
+
+
+
+
 //        session.save(e1);
        // session.save(t1);
 
-        Employee e2 =session.get(Employee.class,(long)1);
-        e2.setDistrict("Sadeghie");
-        session.save(e2);
-            
+//        Employee e2 =session.get(Employee.class,(long)1);
+//        e2.setDistrict("Sadeghie");
+//        session.save(e2);
+
+       // session.save(c1);
+
+
+
         tx.commit();
+        session.close();
+
+//        System.out.println(customers.toString());
+
+//        System.out.println(customers.toString());
+
+//        for (Iterator iterator = map1.iterator(); iterator.hasNext();) {
+//            Map map = (Map) iterator.next();
+//            System.out.println("Working Area: " + map.get("workingArea"));
+//            System.out.println("SUM(): " + map.get("sum"));
+//        }
+
+//        for (Customer c:map){
+//            System.out.println(c.toString());
+//        }
     }
 }
