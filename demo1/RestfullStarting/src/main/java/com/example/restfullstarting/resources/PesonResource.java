a3df1974b2132228b5525a0a39cd7cd5c67bd5ed
zhmmhs <zhmmhs@gmail.com>
2022-02-02 19:46:52 +0330

    Restfull Working...

--- demo1/RestfullStarting/src/main/java/com/example/restfullstarting/resources/PesonResource.java
+++ demo1/RestfullStarting/src/main/java/com/example/restfullstarting/resources/PesonResource.java
@@ -21,7 +21,7 @@ public class PesonResource {
     }
 
     @GetMapping(value ="/{id}" )
-    public Person getById(@PathVariable long id){
+    public Person getById(@PathVariable("id") long id){
            return persRepo.findById(id).get();
         }
 
@@ -31,14 +31,29 @@ public class PesonResource {
         return persRepo.save(person);
     }
 
-    @PutMapping(value = "/{id}",consumes = "applicaton/json")
+    @PutMapping(value = "/{id}",consumes = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public Person update(@RequestBody Person p ){
         return persRepo.save(p);
     }
+
+    @PatchMapping(value = "/{id}",consumes = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public Person partialUpdate(@PathVariable("id") long id  , @RequestBody Person person){
+
+        Person p= persRepo.findById(id).get();
+        if (p.getFirstName()!=null) {
+            p.setFirstName(person.getFirstName());
+        }
+        if (p.getLastName()!=null){
+            p.setLastName(person.getLastName());
+        }
+        return persRepo.save(p);
+    }
+
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable long id){
+    public void delete(@PathVariable("id") long id){
         persRepo.deleteById(id);
     }
 }
